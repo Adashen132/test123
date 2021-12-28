1d2aac617dfe255048ba961d791f4522922cc92c
cyb <cyb@cyb.com>
2021-12-28 21:34:22 +0800

    123

---
+++ src/com/cyb/util/JdbcUtil.java
@@ -0,0 +1,80 @@
+package com.cyb.util;
+
+import java.sql.*;
+// JdbcUtil obj = new JdbcUtil();  obj.getCon()
+// JdbcUtil obj = new JdbcUtil();  obj.createStatement();
+// JdbcUtil.getCon();
+/**
+ * @author ccc
+ * @create 2021-11-16 10:52
+ */
+public class JdbcUtil {
+
+    final String URL="jdbc:mysql://localhost:3306/bjpowernode";
+    final String USERNAME="root";
+    final String PASSWORD="55555";
+    PreparedStatement ps= null;
+    Connection con = null;
+
+    //将jar包中driver实现类加载到JVM中
+    static{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //封装连接通道创建细节
+    public  Connection   getCon(){
+
+        try {
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    //封装交通工具创建细节
+    public  PreparedStatement createStatement(String sql){
+
+        try {
+            ps =  getCon().prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ps;
+    }
+    // ps与con销毁细节 insert,update,delete
+    public  void close(){
+        if(ps!=null){
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(con!=null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    //select ps,con,rs
+    public  void close(ResultSet rs){
+        if(rs!=null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        close();
+    }
+}
